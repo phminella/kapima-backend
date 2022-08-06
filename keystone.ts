6646c969fedfce5fbe5f24f88f58b45d8574a092
minella <phminella@gmail.com>
2022-08-06 22:24:52 +0900

    first

--- keystone.ts
+++ keystone.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import "dotenv/config";
 /*
 Welcome to Keystone! This file is what keystone uses to start the app.
 
@@ -7,43 +7,45 @@ It looks at the default export, and expects a Keystone config object.
 You can find all the config options in our docs here: https://keystonejs.com/docs/apis/config
 */
 
-import { config } from '@keystone-6/core';
+import { config } from "@keystone-6/core";
 
 // Keystone auth is configured separately - check out the basic auth setup we are importing from our auth file.
-import { withAuth, session } from './auth';
+import { withAuth, session } from "./auth";
 // import { statelessSessions } from '@keystone-6/core/session';
 
 // Database URL from .env
 const databaseUrl = process.env.DATABASE_URL || "capybara string";
 const shadowDatabaseUrl = process.env.SHADOW_DATABASE_URL || "oi";
+//@ts-ignore
+const port = parseInt(process.env.PORT) || 3000;
 // lists schema
-import { Product } from './schemas/Products';
-import { User } from './schemas/User';
-import { ProductImage } from './schemas/ProductImage';
-import { CartItem } from './schemas/CartItem';
-import { OrderProduct } from './schemas/OrderProduct';
-import { Order } from './schemas/Order';
+import { Product } from "./schemas/Products";
+import { User } from "./schemas/User";
+import { ProductImage } from "./schemas/ProductImage";
+import { CartItem } from "./schemas/CartItem";
+import { OrderProduct } from "./schemas/OrderProduct";
+import { Order } from "./schemas/Order";
 // extend schema
-import { extendGraphqlSchema } from './mutations/index';
+import { extendGraphqlSchema } from "./mutations/index";
 
 export default withAuth(
   // Using the config function helps typescript guide you to the available options.e
   config({
     server: {
-      cors: { origin: ['https://kapymarket.web.app/'], credentials: true },
-      port: 3000,
+      cors: { origin: ["https://kapymarket.web.app/"], credentials: true },
+      port,
       maxFileSize: 200 * 1024 * 1024,
       healthCheck: true,
     },
     db: {
-      provider: 'postgresql',
+      provider: "postgresql",
       url: databaseUrl,
       shadowDatabaseUrl,
-      onConnect: async context => { },
+      onConnect: async (context) => {},
       // Optional advanced configuration
       // enableLogging: true,
       useMigrations: false,
-      idField: { kind: 'uuid' }
+      idField: { kind: "uuid" },
     },
     lists: {
       User,
@@ -51,7 +53,7 @@ export default withAuth(
       ProductImage,
       CartItem,
       OrderProduct,
-      Order
+      Order,
     },
     extendGraphqlSchema,
     ui: {
@@ -60,6 +62,6 @@ export default withAuth(
         return !!session?.data;
       },
     },
-    session
+    session,
   })
 );
